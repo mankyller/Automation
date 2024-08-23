913efc8b7c4e1e4266365fe44cbdcf42babfc216
Koi McCormick <koi_mccormick@yahoo.com>
2024-08-23 01:08:05 -0700

    Finished Home Page Test Suite

--- DollarShave/src/test/java/HomePageTestSuite.java
+++ DollarShave/src/test/java/HomePageTestSuite.java
@@ -21,11 +21,11 @@ public class HomePageTestSuite {
         new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
-    @Test
-    public void signInButtonLoadCheck() {
-        WebElement signInButton = driver.findElement(By.cssSelector("span.menu__title"));
-        Assert.assertEquals("Sign in button has loaded", "Sign In", signInButton.getText());
-    }
+//    @Test
+//    public void signInButtonLoadCheck() {
+//        WebElement signInButton = driver.findElement(By.cssSelector("span.menu__title"));
+//        Assert.assertEquals("Sign in button has loaded", "Sign In", signInButton.getText());
+//    }
 
     @Test
     public void starterSetButtonLoadCheck() {
@@ -39,11 +39,37 @@ public class HomePageTestSuite {
     public void viewAllButtonOneLoadCheck() {
         WebElement viewAllButtonOne = driver.findElement
                 (By.cssSelector("a.button.button--primary.button--feature-collection")); //View All Bundles Button
-        Assert.assertEquals("First View All has loaded", "View All", viewAllButtonOne.getText());
+
+        Assert.assertEquals("First View All has loaded",
+                "View All", viewAllButtonOne.getText());
 
     }
 
+    @Test
+    public void sixBladePurchaseButtonLoadCheck() {
+        WebElement buyFor10Button = driver.findElement
+                (By.cssSelector("button.button--primary.product-comparison__cta.btn--add-to-cart--quick"));
 
+        Assert.assertTrue("Buy for $10 call to action has loaded",
+                buyFor10Button.getText().contains("$10"));
+    }
+
+    @Test
+    public void shopShaveAidsButtonLoadCheck() {
+        WebElement shopShaveAidsButton = driver.findElement
+                (By.cssSelector("a.button.hero-banner__button.button--large "));
+
+        Assert.assertEquals("Shop Shave Aids call to action has loaded",
+                "SHOP SHAVE AIDS", shopShaveAidsButton.getText());
+    }
+
+    @Test
+    public void shopAllProductsFooterLinkLoadCheck() {
+        WebElement shopAllProductsLink = driver.findElement(By.linkText("Shop All Products"));
+
+        Assert.assertEquals("Shop All Product link is present",
+                "Shop All Products", shopAllProductsLink.getText());
+    }
 
     @AfterClass
     public static void removeDriver() {
